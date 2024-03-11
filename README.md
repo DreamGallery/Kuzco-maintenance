6a61024edc39d6c6522a1ad74f26e2abe2c8ad34
DreamGallery <2511462508@qq.com>
2024-03-11 09:17:39 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Kuzco-maintenance
-Maintenance shell script for Kuzco running
+Maintenance shell script for Kuzco running.  
+The script will auto restart Kuzco worker when meeting `TimeoutError: The operation timed out` or `CUDA error`.  
+In addition, it will also restart Kuzco worker after running continuously for one hour.
+
+## Usage
+### Run
+```
+git clone https://github.com/DreamGallery/Kuzco-maintenance.git
+cd Kuzco-maintenance && chmod +x kz-maint.sh
+nohup ./kz-maint.sh >> start-kz.log 2>&1 &
+```
+The output of the script is in the `start-kz.log` file, and the worker's is in `kz-worker.log`.  
+Old log files will be moved to the `kzlog` folder.
+
+### Stop
+```
+pkill -f "kz-maint.sh" && pkill -f "kuzco"
+```
---
+++ kz-maint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+[[ -d kzlog ]] || mkdir kzlog
+
+while true; do
+        pidlist=$(ps -ef | grep kuzco | grep -v grep | awk '{print $2}')
+        if [[ -n "$pidlist" ]]; then
+                echo "$(date "+%Y-%m-%d %H:%M:%S") Kuzco task pids: $(echo ${pidlist[@]} | tr '\n' ' ')"
+                for pid in $pidlist; do
+                        sudo kill -9 $pid
+                        echo "$(date "+%Y-%m-%d %H:%M:%S") Killing process with PID: $pid"
+                done
+        fi
+
+        sudo kuzco worker start >> kz-worker.log 2>&1 &
+        echo "$(date "+%Y-%m-%d %H:%M:%S") Starting..."
+
+        runtime=0
+        while true; do
+                if [[ `grep -c "TimeoutError: The operation timed out" kz-worker.log` -ne '0' ]]; then
+                        cp kz-worker.log kzlog/kz-worker-$(date +%Y%m%d%H%M%S).log
+                        cat /dev/null > kz-worker.log
+                        echo "$(date "+%Y-%m-%d %H:%M:%S") TimeoutError, preparing to restart..."
+                        break
+                elif [[ `grep -c "CUDA error: " kz-worker.log` -ne '0' ]]; then
+                        sleep 10
+                        cp kz-worker.log kzlog/kz-worker-$(date +%Y%m%d%H%M%S).log
+                        cat /dev/null > kz-worker.log
+                        echo "$(date "+%Y-%m-%d %H:%M:%S") CUDA error, preparing to restart..."
+                        break
+                else
+                        sleep 1
+                        runtime=$(($runtime+1))
+                        echo "$(date "+%Y-%m-%d %H:%M:%S") Running for ${runtime}s"
+                        if [[ "$runtime" -ge '3600' ]]; then
+                                echo "$(date "+%Y-%m-%d %H:%M:%S") Restarting scheduledly..."
+                                break
+                        fi
+                fi
+        done
+done
